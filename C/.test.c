f1e38e7bbe8e4e3a34b96cb352ab6251bcd054f3
DISK-C137 <csd26prime@gmail.com>
2024-07-12 13:40:24 +0700

    go

--- C/.test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("C language\n");
-    print("I will try to learning...!\n");
-    return 0;
-}
--- C/MyProject1/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("CSD26!\n");
-    return 0;
-}
