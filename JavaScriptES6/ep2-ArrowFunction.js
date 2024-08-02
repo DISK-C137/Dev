fe658c49811f33130435502f7a31a7fe87780728
Chinnawat Suwanpasert <csd26prime@gmail.com>
2024-08-02 15:47:59 +0700

    Delete JavaScriptES6 directory

--- JavaScriptES6/ep2-ArrowFunction.js
+++
@@ -1,9 +0,0 @@
-//แบบเดิม
-// function fullname(fname,lname){
-//     return fname+" "+lname
-// }
-
-//Arrow Function
-fullname=(fname,lname)=>fname+" "+lname
-
-console.log(fullname("Chinnawat","Suwanpasert"))
